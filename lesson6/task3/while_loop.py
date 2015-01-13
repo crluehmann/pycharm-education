73c720a72740547fd47114d6c26604609c8320a1
Chris <crluehmann@gmail.com>
2015-01-13 18:55:17 -0500

    adding!

---
+++ lesson6/task3/while_loop.py
@@ -0,0 +1,15 @@
+count = 1
+
+while count <= 10:
+    print(count)    # This code is executed 10 times
+    count += 1      # This code is executed 10 times
+
+print("Finished")  # This code is executed once
+
+count = 0
+number = 1
+
+while number < 10:
+    count = number ** 2
+    print(count)
+    number += 1
